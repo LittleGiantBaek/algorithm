df006ede8d6307d92e973749836d0ccdb55dcffc
LittleGiantBaek <horowitz@hanmail.net>
2018-07-10 21:52:42 +0900

    baekJoon_1260 테스트케이스는 맞지만 결과 틀림

--- AlgorithmPractice/src/baekJoon_1260/Main.java
+++ AlgorithmPractice/src/baekJoon_1260/Main.java
@@ -1,22 +1,41 @@
 package baekJoon_1260;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
-	public static int reVD[];
-	public static int reVB[];
-	public static int[][] map;
-	public static int cnt = 0;
+	public static int[][] mapD;
+	public static int[][] mapB;
 	
 	public static void dfs(int y, int x) {
-		reVD[cnt++] = x;
-		for(int i = 0; i<map[y].length;i++) {
-			map[y][i] = 0;
-			map[i][y] = 0;
+		System.out.print(x+" ");
+		for(int i = 0; i<mapD[y].length;i++) {
+			mapD[y][i] = 0;
+			mapD[i][y] = 0;
 		}
-		for(int i = 0; i< map[y].length;i++) {
-			if(map[x][i] == 1) {
+		for(int i = 0; i< mapD[y].length;i++) {
+			if(mapD[x][i] == 1) {
 				dfs(x,i);
+			}
+		}
+	}
+	
+	public static void bfs(int y) {
+		Queue<Integer> queue = new LinkedList<>();
+		queue.add(y);
+		while(!queue.isEmpty()) {
+			int next = queue.poll();
+			System.out.print(next+" ");
+			for( int i =0 ;i<mapB.length ; i++) {
+				if(mapB[next][i] == 1) {
+					queue.add(i);
+					for(int j = 0 ; j<mapB.length;j++) {
+						mapB[j][i] = 0;
+					}
+					mapB[next][i] = 0;
+					mapB[i][next] = 0;
+				}
 			}
 		}
 	}
@@ -26,30 +45,27 @@ public class Main {
 		int N = sc.nextInt();
 		int M = sc.nextInt();
 		int startV = sc.nextInt();
-		map = new int[N+1][N+1];
-		reVD = new int[N+1];
-		reVB = new int[N+1];
-		reVD[cnt++] = startV;
-		reVB[cnt++] = startV;
+		mapD = new int[N+1][N+1];
+		mapB = new int[N+1][N+1];
+		
 		for (int i = 0; i < M; i++) {
 			int fv = sc.nextInt();
 			int lv = sc.nextInt();
-			map[fv][lv] = 1;
-			map[lv][fv] = 1;
+			mapD[fv][lv] = 1;
+			mapD[lv][fv] = 1;
+			mapB[fv][lv] = 1;
+			mapB[lv][fv] = 1;
 		}
 
 		for (int i = 0; i < N; i++) {
-			if (map[startV][i] == 1) {
+			if (mapD[startV][i] == 1) {
+				System.out.print(startV+" ");
 				dfs(startV, i);
-			}
-		}
-		for(int i =0; i< reVD.length;i++) {
-			if(reVD[i] != 0) {
-				System.out.print(reVD[i]+" ");
 			}
 		}
 		System.out.println();
 		
+		bfs(startV);
 		
 	}
 }
